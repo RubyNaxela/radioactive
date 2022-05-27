26484af4821b2d0e7de7ecf072090a3ecc9234d1
RubyNaxela <rubynaxela@gmail.com>
2022-05-27 20:49:29 +0200

    Placeholder sea

--- src/com/kyaniteteam/radioactive/GameScene.java
+++ src/com/kyaniteteam/radioactive/GameScene.java
@@ -1,12 +1,13 @@
 package com.kyaniteteam.radioactive;
 
 import com.rubynaxela.kyanite.game.Scene;
+import com.rubynaxela.kyanite.util.Colors;
 
 public class GameScene extends Scene {
 
     @Override
     protected void init() {
-
+        setBackgroundColor(Colors.DARK_BLUE);
     }
 
     @Override
