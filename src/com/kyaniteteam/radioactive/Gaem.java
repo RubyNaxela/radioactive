f1676c9610c58d1d3d953d65f1b7e2d52e2a3460
RubyNaxela <rubynaxela@gmail.com>
2022-05-27 18:20:24 +0200

    Initial commit

---
+++ src/com/kyaniteteam/radioactive/Gaem.java
@@ -0,0 +1,20 @@
+package com.kyaniteteam.radioactive;
+
+import com.rubynaxela.kyanite.game.Game;
+
+public class Gaem extends Game {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+
+    @Override
+    protected void preInit() {
+
+    }
+
+    @Override
+    protected void init() {
+
+    }
+}
