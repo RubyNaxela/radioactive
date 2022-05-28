6e78efe9acce8c5820548dde11493639efc481d2
RubyNaxela <rubynaxela@gmail.com>
2022-05-28 22:13:45 +0200

    Waves shader added

--- src/com/kyaniteteam/radioactive/GameScene.java
+++ src/com/kyaniteteam/radioactive/GameScene.java
@@ -2,27 +2,23 @@ package com.kyaniteteam.radioactive;
 
 import com.kyaniteteam.radioactive.entities.DroppedBarrel;
 import com.kyaniteteam.radioactive.entities.boats.EnemyBoat;
-import com.kyaniteteam.radioactive.entities.boats.PatrolBoat;
 import com.kyaniteteam.radioactive.entities.boats.PlayerBoat;
-import com.kyaniteteam.radioactive.entities.boats.PoliceBoat;
 import com.kyaniteteam.radioactive.terrain.Depth;
 import com.rubynaxela.kyanite.game.GameContext;
 import com.rubynaxela.kyanite.game.Scene;
 import com.rubynaxela.kyanite.game.assets.AudioHandler;
-import com.rubynaxela.kyanite.util.Vec2;
 import com.rubynaxela.kyanite.window.Window;
 import org.jetbrains.annotations.NotNull;
 import org.jsfml.graphics.Color;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 public class GameScene extends Scene {
 
     private static final AudioHandler audioHandler = GameContext.getInstance().getAudioHandler();
     private final Window window = getContext().getWindow();
-//    private final Background background = new Background();
+    //    private final Background background = new Background();
     private final PlayerBoat player = new PlayerBoat(this);
 
 //    private final PatrolBoat patrolBoat = new PatrolBoat(this, 150, 30);
@@ -34,7 +30,7 @@ public class GameScene extends Scene {
 
     public GameScene(@NotNull SceneLoader.SceneData data) {
         add(data.enemies.stream().map(e -> e.createEnemyBoat(this)).toList());
-        setBackgroundColor(new Color());
+        setBackgroundColor(new Color(83, 85, 158));
     }
 
     public PlayerBoat getPlayer() {
@@ -43,17 +39,17 @@ public class GameScene extends Scene {
 
     public List<DroppedBarrel> getBarrels() {
         return drawables.stream().filter(b -> b instanceof DroppedBarrel)
-                .map(b -> (DroppedBarrel) b).collect(Collectors.toList());
+                        .map(b -> (DroppedBarrel) b).collect(Collectors.toList());
     }
 
     public List<EnemyBoat> getEnemyBoats() {
         return drawables.stream().filter(b -> b instanceof EnemyBoat)
-                .map(b -> (EnemyBoat) b).collect(Collectors.toList());
+                        .map(b -> (EnemyBoat) b).collect(Collectors.toList());
     }
 
     public List<Depth> getDepths() {
         return drawables.stream().filter(d -> d instanceof Depth)
-                .map(d -> (Depth) d).collect(Collectors.toList());
+                        .map(d -> (Depth) d).collect(Collectors.toList());
     }
 
     @Override
